ddc59d504c06451d1cc5b193e9ffb6c3ffa48189
torobucci <kevintoro254@gmail.com>
2023-03-16 17:38:19 +0300

    add more styling to browser

--- webpack.config.js
+++ webpack.config.js
@@ -20,11 +20,21 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    assetModuleFilename: 'img/[name][ext][query]',
   },
-  mode: 'development',
+  mode: 'production',
 };
